7e6783de429321aa6f5caad22ecbeb5e46473bbe
richmtx <martinezhernandezricardo06@gmail.com>
2026-01-13 12:47:53 -0600

    Commit

---
+++ js/eventos.js
@@ -0,0 +1,20 @@
+
+const revealEvents = document.querySelectorAll('.reveal-event');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.2
+    }
+);
+
+revealEvents.forEach(event => {
+    observer.observe(event);
+});
