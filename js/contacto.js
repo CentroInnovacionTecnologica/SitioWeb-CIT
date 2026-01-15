1acfa429bbc40468c2df5cf340ab97f14898ec32
richmtx <martinezhernandezricardo06@gmail.com>
2026-01-15 13:47:29 -0600

    Commit

---
+++ js/contacto.js
@@ -0,0 +1,16 @@
+
+// Animación Contacto
+const elements = document.querySelectorAll('.scroll-animate');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+elements.forEach(el => observer.observe(el));
