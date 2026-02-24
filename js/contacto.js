ee3f15f8a1fa435011d3b3d794bcacc883609b3b
Lupita <guadalupe.hdz.contre@gmail.com>
2026-02-24 10:42:00 -0600

    cambios

--- js/contacto.js
+++ js/contacto.js
@@ -1,16 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-// Animación Contacto
-const elements = document.querySelectorAll('.scroll-animate');
+  const elements = document.querySelectorAll(".scroll-animate");
 
-const observer = new IntersectionObserver((entries) => {
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            observer.unobserve(entry.target);
-        }
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
     });
-}, {
-    threshold: 0.15
-});
+  }, {
+    threshold: 0.2
+  });
 
-elements.forEach(el => observer.observe(el));
+  elements.forEach(el => observer.observe(el));
+
+});
